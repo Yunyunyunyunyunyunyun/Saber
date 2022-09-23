5a78bf50d6291a8c817e5ed641175cf30d16f96b
wangguoyun <wendywgyy@gmail.com>
2022-09-23 11:46:21 +0800

    重启病例

--- src/views/case/restartCaseDetail.vue
+++ src/views/case/restartCaseDetail.vue
@@ -8,6 +8,31 @@
       <el-tabs v-model="active">
         <el-tab-pane :name="1">
           <span slot="label"><i class="el-icon-chat-dot-square"></i> 阶段反馈<i class="el-icon-arrow-right arrow-style"></i></span>
+          <div class="restart-case-main-one">
+            <div class="restart-case-main-one-top">
+              <div class="restart-case-main-one-top-img">
+                <img v-if="caseItem.photo && caseItem.photo.frontPath" :src="caseItem.photo.frontPath" alt="" class="top-img">
+                <i v-else class="el-icon-user top-icon"></i>
+              </div>
+              <div class="restart-case-main-one-top-name" v-if="caseItem.prescription && caseItem.prescription.name">
+                <span :title="caseItem.prescription.name">{{caseItem.prescription.name}}</span>
+              </div>
+              <div class="restart-case-main-one-top-id">
+                <span>病历号：</span>
+                <span v-if="caseItem.record && caseItem.record.medicalCode" class="restart-case-main-one-top-id-span">{{caseItem.record.medicalCode}}</span>
+              </div>
+            </div>
+            <el-form :model="feedbackForm" :rules="feedbackRules" ref="feedbackForm" label-position="top">
+              <div class="diagnosis-title diagnosis-title-required">1. 当前矫治器贴合情况<span class="diagnosis-tip">判断依据：牙齿移动及咬合关系与模拟矫治设计基本符合，矫治器与牙齿之间无明显空隙</span></div>
+              <el-form-item prop="isFit">
+                <el-radio-group v-model="feedbackForm.isFit" class="common-select">
+                  <el-radio :label="1" border>矫治器贴合</el-radio>
+                  <el-radio :label="2" border>矫治器不贴合</el-radio>
+                </el-radio-group>
+                <div class="not-fit-desc" :class="{'hide-desc': feedbackForm.isFit !== 2}">需提交全口硅橡胶印膜，或全口数字模型文件</div>
+              </el-form-item>
+            </el-form>
+          </div>
         </el-tab-pane>
         <el-tab-pane :name="2">
           <span slot="label"><i class="el-icon-video-camera"></i> 影像资料及模型<i class="el-icon-arrow-right arrow-style"></i></span>
@@ -32,7 +57,25 @@
     data() {
       return {
         active: 1,
+        caseItem: {},
+        feedbackForm: {
+          isFit: "",
+        },
+        feedbackRules: {},
       }
+    },
+    created() {
+      var rParamsData = sessionStorage.getItem("rParamsData");
+      if (rParamsData) {
+        var rParams = JSON.parse(sessionStorage.getItem("rParamsData"));
+      } else {
+        var rParams = this.$route.params;
+        sessionStorage.setItem("rParamsData", JSON.stringify(rParams));
+      }
+      this.caseItem = rParams.item;
+    },
+    beforeDestroy() {
+      sessionStorage.removeItem("rParamsData");
     },
     methods: {
       back() {
@@ -102,5 +145,103 @@
     background: #fff;
     box-shadow: 0 2px 14px 0 rgb(221 225 233 / 54%);
     padding: 11px 0;
+  }
+  .restart-case-main-one {
+    padding: 30px 0 0 40px;
+  }
+  .restart-case-main-one-top {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    margin-bottom: 60px;
+  }
+  .restart-case-main-one-top-img {
+    width: 82px;
+    height: 82px;
+  }
+  .top-img {
+    width: 82px;
+    height: 82px;
+    border-radius: 50%;
+  }
+  .top-icon {
+    font-size: 82px;
+  }
+  .restart-case-main-one-top-name {
+    margin-left: 30px;
+    white-space: nowrap;
+    font-weight: 400;
+    font-size: 26px;
+    color: #333;
+    max-width: 500px;
+    overflow: hidden;
+    word-break: break-all;
+    text-overflow: ellipsis;
+  }
+  .restart-case-main-one-top-id {
+    margin-left: 30px;
+    font-weight: 300;
+    font-size: 18px;
+    color: #999;
+  }
+  .restart-case-main-one-top-id-span {
+    font-weight: 400;
+    font-size: 18px;
+    color: #555;
+  }
+  .diagnosis-title {
+    font-size: 20px;
+    color: #555;
+    height: 30px;
+    line-height: 30px;
+    margin-bottom: 24px;
+  }
+  .diagnosis-title-required::before {
+    content: '*';
+    color: #F56C6C;
+    margin-right: 4px;
+  }
+  .diagnosis-tip {
+    font-size: 16px;
+    margin-left: 20px;
+    color: #999;
+  }
+  .common-select >>> .el-radio__input {
+    display: none;
+  }
+  .common-select >>> .el-radio.is-bordered {
+    text-align: center;
+  }
+  .common-select >>> .el-radio.is-bordered.is-checked {
+    border-color: #409EFF;
+    background: #409EFF;
+  }
+  .common-select >>> .el-radio__input.is-checked+.el-radio__label {
+    color: #fff;
+  }
+  .common-select >>> .el-checkbox__input {
+    display: none;
+  }
+  .common-select >>> .el-checkbox.is-bordered {
+    width: 120px;
+    margin-left: 0;
+    margin-bottom: 15px;
+    text-align: center;
+  }
+  .common-select >>> .el-checkbox.is-bordered.is-checked {
+    border-color: #409EFF;
+    background: #409EFF;
+  }
+  .common-select >>> .el-checkbox__input.is-checked+.el-checkbox__label {
+    color: #fff;
+  }
+  .not-fit-desc {
+    color: #f44336;
+    font-size: 14px;
+    font-weight: 400;
+    margin-left: 152px;
+  }
+  .hide-desc {
+    opacity: 0;
   }
 </style>
